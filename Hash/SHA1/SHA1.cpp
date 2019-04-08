24c6a34a7ec59559c1c820f22a03805b698c25cc
lzb991435344 <991435344@qq.com>
2019-04-09 00:13:36 +0800

    Hash加解密过程

---
+++ Hash/SHA1/SHA1.cpp
@@ -0,0 +1,16 @@
+// SHA1.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "SHA1.h"
+
+SHA1::SHA1() {}
+SHA1::~SHA1() {
+	
+}
+
+int main()
+{
+    return 0;
+}
+
---
+++ Hash/SHA1/SHA1.h
@@ -0,0 +1,44 @@
+#ifndef _SHA1_H_
+#define _SHA1_H_
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+
+/**
+*	usage    :    char SHABuffer[41];
+*                    SHA1 SHA;
+*                    SHA.SHA_GO("gfc",SHABuffer);
+*                    执行完成之后SHABuffer中即存储了由"a string"计算得到的SHA值
+*	算法描述：
+*		1.填充形成整512 bits(64字节), (N+1)*512;
+*		 N*512 + 448位                         64位
+*		真实数据 + 填充(一个1和无数个0)                 保存真实数据的长度
+*		—————————————————————— ----------------------
+*		2.四个32位被称为连接变量的整数参数：
+*		A=，B=，C=，D=,E=
+*		3.开始算法的四轮循环运算。
+*		循环的次数(N+1)是信息中512位信息分组的数目。
+*		for(N+1, 每次取512 bits(64字节)来进行处理：)
+*		{
+*		512bits 分成16个分组(每个分组4字节。int data[16])
+*		用16个分组中的每一个分组和A，B，C，D,E通过4个函数进行运算，
+*		得到新的A，B，C，D,E
+*		}
+*		4.最后，将A，B，C，D,E拼接起来就是SHA值
+*		将拼接后的值用字符串的方式表示出来，就是最终的结果。
+*		(比如 0x234233F, 用串 “234233F”表示出来，生成的串的空间将是值的空间的2倍。)
+*/
+class SHA1
+{
+public:
+	SHA1();
+	virtual ~SHA1();
+protected:
+private:
+
+private:
+
+
+};
+#endif
